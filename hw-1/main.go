4a814305e079383b790e2d56c38abb9e8eb4a984
verter187 <verter187@inbox.ru>
2023-05-21 16:57:44 +0300

    homework for lesson 3

--- hw-1/main.go
+++ hw-1/main.go
@@ -1,28 +1,46 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
-func sum(a, b int) int {
-	return a + b
+func isNumeric(s string) bool {
+	_, err := strconv.ParseFloat(s, 64)
+	return err == nil
 }
+
+func strUnpack(str string) (string, error) {
+	var ns string
+	prev := ""
+	for _, s := range str {
+
+		cs := string(s)
+		if num, err := strconv.Atoi(string(s)); err == nil {
+			if prev == "" || isNumeric(prev) {
+				return "", errors.New("invalid string")
+			} else {
+				cs = strings.Repeat(prev, num-1) //already printed once
+			}
+		}
+
+		prev = string(s)
+		ns += cs
+	}
+
+	return ns, nil
+}
+
 func main() {
-	fmt.Println(sum(5, 7))
 
-	//Не инициализированный map равен nil
-	var c map[int]string
-	fmt.Println(c == nil)
-
-	//Инициализированный map не равен nil
-	var c1 map[int]string = make(map[int]string)
-	fmt.Println(c1 == nil)
-
-	//Не инициализированный slice равен nil
-	var s []string
-	fmt.Println(s == nil)
-
-	//Инициализированный slice не равен nil
-	var s1 []string = []string{}
-	fmt.Println(s1 == nil)
+	// * "a4bc2d5e" => "aaaabccddddde"
+	// * "abcd" => "abcd"
+	// * "45" => "" (некорректная строка)
+	if test, err := strUnpack("45"); err == nil {
+		fmt.Println(test)
+	} else {
+		fmt.Println(err)
+	}
 }
