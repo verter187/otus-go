8d79c6a3af66aa6068f2471ff756806343a1a97a
verter187 <verter187@inbox.ru>
2023-06-01 19:26:56 +0300

    Занятие5.Functions

--- hw-1/main.go
+++ hw-1/main.go
@@ -2,59 +2,89 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"sort"
-	"strings"
-	"unicode/utf8"
 )
 
-func Dedupe[T comparable](arr []T) map[T]int {
-	m := make(map[T]int)
-	for _, v := range arr {
-		m[v]++
-	}
-	return m
-}
+// области видимости, блоки
+// функции: вариадические, анонимные, замыкания, методы
+// ошибки: принципы обработки, best practices
+// panic, recover, defer
 
+// Области видимости и блоки
+// Блок (block) - это, возможно пустая, последовательность объявлений (declarations) и операторов (statements) в соответствующих фигурных скобках
+
+// - universe block - весь код проекта
+// - package block - весь код пакета
+// - file block - исходный код в файле
+// - local block - просто {}
 func main() {
-	// для более мелких файлов
-	filename := "./anna1.txt" //os.Args[1] //"./temp/anna1.txt"
-	fContent, err := os.ReadFile(filename)
-	if err != nil {
-		panic(err)
+	{ // yначало блока 1
+		a := 1
+		fmt.Println(a)
+		{ // начало блока 2
+			b := 2
+			fmt.Println(b)
+		} // конец блока 2
+	} // конец блока 1
+
+	// Неявные блоки: for, if, switch, case, select
+
+	// {
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+	//}
+
+	if i := 0; i >= 0 {
+		fmt.Println(i)
 	}
 
-	replace_marks_str := "!,.?:;\"-[]()123456789"
-	replace_marks := strings.SplitAfter(replace_marks_str, "")
-	content := string(fContent)
+	switch i := 2; i * 4 {
+	case 8:
+		j := 0
+		fmt.Println(i, j)
+	default:
+		// "j" is undefined here
+		fmt.Println("default")
+	} // "j" is undefined here
 
-	for _, mark := range replace_marks {
-		content = strings.ReplaceAll(content, mark, "")
-	}
+	// Интересное:
+	// * Нет дефолтных значений для параметров
+	// * функция может возвращать несколько значений
+	// * функция - first class value, можем работать как с обычным значением
+	// * параметры в функцию передаются по значению
 
-	words := strings.Fields(content)
+	func(text string) {
+		fmt.Println(text)
+	}("test777")
 
-	fmt.Println(len(words))
-	mWords := Dedupe(words)
+	sum := func(a ...int) int {
 
-	names := make([]string, 0, len(mWords))
-	for name := range mWords {
-
-		if utf8.RuneCountInString(strings.Trim(name, " ")) < 4 {
-			continue
+		s := 0
+		for _, i := range a {
+			s += i
 		}
-
-		names = append(names, name)
+		return s
 	}
+	s := sum(1, 2, 3, 4, 5)
+	fmt.Println(s)
 
-	sort.Slice(names, func(i, j int) bool {
-		return mWords[names[i]] > mWords[names[j]]
-	})
+	p := []int{1, 2, 3, 4, 5, 6}
+	s1 := sum(p...)
+	fmt.Println(s1)
 
-	for i, name := range names {
-		if i > 9 {
-			break
+	// Замыкания
+
+	intSeq := func() func() int {
+		i := 0
+		return func() int {
+			i++
+			return i
 		}
-		fmt.Printf("%-7v %v\n", name, mWords[name])
 	}
+
+	nextInt := intSeq()
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
 }
